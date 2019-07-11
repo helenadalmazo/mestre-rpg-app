a1e13ff058e0b5555eadbff837b86414dee51ece
Helena Dalmazo <helenadalmazo@outlook.com>
2019-07-11 00:51:55 -0300

    Adicionando extra NPC_IMAGE_ACTION

--- app/src/main/java/com/dalmazo/helena/mestrerpg/util/Extra.kt
+++ app/src/main/java/com/dalmazo/helena/mestrerpg/util/Extra.kt
@@ -6,5 +6,6 @@ class Extra {
         const val NPC_OBJECT: String = "EXTRA_NPC_OBJECT"
         const val NPC_ACTION: String = "EXTRA_NPC_ACTION"
         const val NPC_IMAGE: String = "EXTRA_NPC_IMAGE"
+        const val NPC_IMAGE_ACTION: String = "EXTRA_NPC_IMAGE_ACTION"
     }
 }
